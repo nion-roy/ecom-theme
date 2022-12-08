833b9cf357940c2e52f1ea4b40091087bf8990d9
Nion Roy <nion.roy22@gmail.com>
2022-12-08 13:28:10 +0600

    new added

--- asstes/js/scrollReveal.js
+++ asstes/js/scrollReveal.js
@@ -6,10 +6,8 @@ ScrollReveal({
    delay: 400,
 });
 
-ScrollReveal().reveal(".header-section", { delay: 500, origin: "left" });
-ScrollReveal().reveal(".header_wrap", { delay: 500, origin: "bottom" });
 ScrollReveal().reveal(".header_content_box .headline_title", { delay: 500, origin: "left" });
 ScrollReveal().reveal(".header_content_box .headline_button", { delay: 500, origin: "right" });
 ScrollReveal().reveal(".box, .copyright span, .copyright a", { delay: 500, origin: "bottom", interval: 200 });
-ScrollReveal().reveal(".about_wrap img, .left_item", { delay: 500, origin: "top" });
-ScrollReveal().reveal(".about_wrap .text-center, .right_item", { delay: 500, origin: "bottom" });
+ScrollReveal().reveal(".left, .about_wrap img, .left_item", { delay: 500, origin: "top" });
+ScrollReveal().reveal(".right, .about_wrap .text-center, .right_item", { delay: 500, origin: "bottom" });
